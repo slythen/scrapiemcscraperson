d2e41680d72ab9ce115a655ef61eac29476100f2
slythen <m_ride_flock@q.com>
2014-04-02 08:42:00 -0800

    version 3.0

--- 3.py
+++ 3.py
@@ -7,34 +7,60 @@ choiceP = str("http://www.directv.com/DTVAPP/compare/printablePackageChannels.js
 xtraP = str("http://www.directv.com/DTVAPP/compare/printablePackageChannels.jsp?packageId=960010&skuId=sku930012&referrer=https://support.directv.com/app/answers/detail/a_id/360/kw/printable%20channel%20list")
 ultimateP = str("http://www.directv.com/DTVAPP/compare/printablePackageChannels.jsp?packageId=1120008&skuId=sku1140004&referrer=https://support.directv.com/app/answers/detail/a_id/360/kw/printable%20channel%20list")
 premierP = str("http://www.directv.com/DTVAPP/compare/printablePackageChannels.jsp?packageId=960014&skuId=sku930016&referrer=https://support.directv.com/app/answers/detail/a_id/360/kw/printable%20channel%20list&referrer=https://support.directv.com/app/answers/detail/a_id/360/kw/printable%20channel%20list" )
+  
+urls = [selectP, entertainmentP, choiceP, xtraP, ultimateP, premierP]
 
+def packAger(urL):
+    if urL == selectP:
+        Package = 'SelectP'
+    elif urL == entertainmentP:
+        Package = 'EntertainmentP'
+    elif urL == choiceP:
+        Package = 'ChoiceP'
+    elif urL == xtraP:
+        Package = 'xtraP'
+    elif urL == ultimateP:
+        Package = 'ultimateP'
+    elif urL == premierP:
+        Package = 'premierP'
+    else:
+        Package = 'Crap'
+    return Package
 
-r = requests.get(premierP)
-soup = BeautifulSoup(r.text)
-
-def overTable():
+for packageUrl in urls:
+    r = requests.get(packageUrl)
+    soup = BeautifulSoup(r.text)
     RoW = []
     MainT = soup.find_all('table', class_="package")
     for TablE in MainT:
-        RoW.append(TablE)
-    return RoW
+        RoW.append(TablE)    
+    TableS = RoW
+    tableS = BeautifulSoup(str(TableS).strip(']').strip('['))
+    UberDict = {}
+    rowNum = 0
+    rows = len(tableS.find_all('tr'))
+    while (rowNum < rows):
+        cell = tableS.find_all('tr')[rowNum]
+        UberDict[str(cell.find('td', width='98%').text).strip().strip('*')] = [str(cell.find('td', class_="ch-no").text).strip()]
+        rowNum += 1
 
-TableS = overTable()
+for currentPack in urls:
+    r = requests.get(packageUrl)
+    soup = BeautifulSoup(r.text)
+    RoW = []
+    MainT = soup.find_all('table', class_="package")
+    for TablE in MainT:
+        RoW.append(TablE)    
+    TableS = RoW
+    tableS = BeautifulSoup(str(TableS).strip(']').strip('['))
+    rowNum = 0
+    rows = len(tableS.find_all('tr'))
+    while (rowNum < rows):
+        cell = tableS.find_all('tr')[rowNum]
+        UberDict[str(cell.find('td', width='98%').text).strip().strip('*')].append(packAger(currentPack))        
+        rowNum += 1
 
-
-tableS = BeautifulSoup(str(TableS).strip(']').strip('['))
-
-UberDict = {}
-
-tableS.tr.next_sibling
-rowNum = 0
-rows = len(tableS.find_all('tr'))
-while (rowNum < rows):
-    cell = tableS.find_all('tr')[rowNum]
-    UberDict[str(cell.find('td', width='98%').text).strip()] = [str(cell.find('td', class_="ch-no").text).strip()]
-    rowNum += 1
-
-
+print UberDict
 
 
 #rowS = []
